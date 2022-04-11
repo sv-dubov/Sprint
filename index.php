aa3e0bb7e73424a125bbff8574b68eb8e2ffbedf
Sviatoslav <sv_dubov@ukr.net>
2022-04-11 12:58:07 +0300

    Fix buttons in table

--- index.php
+++ index.php
@@ -27,15 +27,17 @@ include 'backend/database.php';
                                 <div class="col-sm-6">
                                     <h6 class="mr-2"><span>Users</span></h6>
                                 </div>
-                                <div class="col-sm-6">
-                                    <a id="addUser" class="btn btn-success" data-toggle="modal">Add user</a>
-                                    <select class="form-control" id="actions" style="width:auto;">
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="row">
+                                    <select class="form-control mr-3" id="actions" style="width:auto;">
                                         <option selected="selected" value="0" disabled>Please Select</option>
                                         <option value="1">Set active</option>
                                         <option value="2">Set not active</option>
                                         <option value="3">Delete</option>
                                     </select>
-                                    <button type="button" class="btn btn-info" id="btn-actions">OK</button>
+                                    <button type="button" class="btn btn-info mr-5" id="btn-actions">OK</button>
+                                    <a id="addUser" class="btn btn-success" data-toggle="modal">Add user</a>
                                 </div>
                             </div>
                             <div class="e-table">
